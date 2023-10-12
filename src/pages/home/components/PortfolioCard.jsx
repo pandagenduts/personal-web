dffee611d2b1739fe18fe57ee8f86c52db3d1356
Donny Rendi <donny.rendi@yahoo.co.id>
2023-10-12 13:56:59 +0700

    migrating to a component done; progress on card click set state on redux based on card data

--- src/pages/home/components/PortfolioCard.jsx
+++ src/pages/home/components/PortfolioCard.jsx
@@ -1,10 +1,18 @@
-const PortfolioCard = () => {
+import { useDispatch } from "react-redux";
+
+const PortfolioCard = ({itemData}) => {
+  const {title, description, imageCover, projectURL, techExplanation} = itemData
+  const dispatch = useDispatch();
+
+  
+
   return (
     <div className="relative cursor-pointer overflow-hidden rounded-xl">
-      <img src={Luxe} className="object-contain" />
-      <div className="absolute left-0 top-0 z-10 flex h-full w-full items-center justify-center bg-slate-900 bg-opacity-0 opacity-0 duration-300 hover:bg-opacity-40 hover:opacity-100">
-        <button className="rounded-full bg-bluePrimary px-4 py-2 text-white">
-          Space Tourism
+      <img src={imageCover} className="object-contain" />
+      <div className="absolute left-0 top-0 z-10 flex h-full w-full flex-col items-center justify-center bg-slate-900 bg-opacity-0 opacity-0 duration-300 hover:bg-opacity-40 hover:opacity-100">
+        {title && <p className="font-montserrat text-xl text-white">{title}</p>}
+        <button className="rounded-full bg-bluePrimary px-4 py-2 text-sm text-white">
+          See Preview
         </button>
       </div>
     </div>
--- src/pages/home/components/PortoReact.jsx
+++ src/pages/home/components/PortoReact.jsx
@@ -14,16 +14,16 @@ const projectDatas = [
   },
   {
     title: "Luxe",
-    imageCover: SpaceTourism,
+    imageCover: Luxe,
     projectURL: "https://pandagenduts-space-tourism-2.netlify.app/",
-    description: "lorem ipsum dolor sit amet",
+    description: "Luxe ipsum dolor sit amet",
     techExplanation: "wordpress elementor custom HTML CSS JS",
   },
   {
     title: "Brands io",
-    imageCover: SpaceTourism,
+    imageCover: BrandsIo,
     projectURL: "https://pandagenduts-space-tourism-2.netlify.app/",
-    description: "lorem ipsum dolor sit amet",
+    description: "Brands io ipsum dolor sit amet",
     techExplanation: "wordpress elementor custom HTML CSS JS",
   },
 ];
@@ -32,11 +32,11 @@ const PortoReact = () => {
 
   return (
     <>
-      {/* {projectDatas.map(item => (
-        <PortfolioCard itemData={} />
+      {projectDatas.map((item, index) => (
+        <PortfolioCard key={index} itemData={item} />
 
-      ))} */}
-      <div className="relative cursor-pointer overflow-hidden rounded-xl">
+      ))}
+      {/* <div className="relative cursor-pointer overflow-hidden rounded-xl">
         <img src={Luxe} className="object-contain" />
         <div className="absolute left-0 top-0 z-10 flex flex-col h-full w-full items-center justify-center bg-slate-900 bg-opacity-0 opacity-0 duration-300 hover:bg-opacity-40 hover:opacity-100">
           <p className="text-white text-xl font-montserrat">LUXe</p>
@@ -44,7 +44,7 @@ const PortoReact = () => {
             See Preview
           </button>
         </div>
-      </div>
+      </div> */}
     </>
   );
 };
