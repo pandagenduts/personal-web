bbe5b010bce9361e9af47bbcbaf58546ea34c622
Donny Rendi <donny.rendi@yahoo.co.id>
2023-10-12 13:37:31 +0700

    portfolio cards responsive done

--- src/pages/home/components/Portfolio.jsx
+++ src/pages/home/components/Portfolio.jsx
@@ -6,18 +6,41 @@ import PortoHTML from "./PortoHTML";
 const Portfolio = () => {
   const [cardCategory, setCardCategory] = useState("REACT");
 
+  const buttonClasses = "duration-150 hover:text-bluePrimary";
+
   return (
     <section className="container w-full px-4">
       <h2 className="text-center">Portfolio</h2>
-      <div className="flex justify-center gap-4">
-        <button onClick={() => setCardCategory('REACT')}>React & Next JS</button>
-        <button onClick={() => setCardCategory('WORDPRESS')}>WordPress</button>
-        <button onClick={() => setCardCategory('HTML')}>HTML CSS JS</button>
+      <div className="flex justify-center gap-4 mb-10">
+        <button
+          onClick={() => setCardCategory("REACT")}
+          className={`${buttonClasses} ${
+            cardCategory === "REACT" ? "text-bluePrimary" : ""
+          }`}
+        >
+          React & Next JS
+        </button>
+        <button
+          onClick={() => setCardCategory("WORDPRESS")}
+          className={`${buttonClasses} ${
+            cardCategory === "WORDPRESS" ? "text-bluePrimary" : ""
+          }`}
+        >
+          WordPress
+        </button>
+        <button
+          onClick={() => setCardCategory("HTML")}
+          className={`${buttonClasses} ${
+            cardCategory === "HTML" ? "text-bluePrimary" : ""
+          }`}
+        >
+          HTML CSS JS
+        </button>
       </div>
-      <div className="grid h-auto w-full grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 ">
-        {cardCategory === 'REACT' && <PortoReact />}
-        {cardCategory === 'WORDPRESS' && <PortoWP />}
-        {cardCategory === 'HTML' && <PortoHTML />}
+      <div className="grid h-auto  grid-cols-1 gap-4 sm:w-full sm:grid-cols-2 lg:grid-cols-3 ">
+        {cardCategory === "REACT" && <PortoReact />}
+        {cardCategory === "WORDPRESS" && <PortoWP />}
+        {cardCategory === "HTML" && <PortoHTML />}
       </div>
     </section>
   );
--- src/pages/home/components/PortoReact.jsx
+++ src/pages/home/components/PortoReact.jsx
@@ -15,13 +15,13 @@ const projectDatas = [
 
 const PortoReact = () => {
   const cardClasses =
-    "min-h-[400px] rounded-xl overflow-hidden relative cursor-pointer";
+    "rounded-xl overflow-hidden relative cursor-pointer";
   const cardOverlayClasses = "opacity";
 
   return (
     <>
       <div className={`${cardClasses} bg-slate-500`}>
-        <img src={Luxe} className="h-full w-full object-cover" />
+        <img src={Luxe} className="object-contain" />
         <div
           className={`${cardOverlayClasses} absolute left-0 top-0 z-10 flex h-full w-full items-center justify-center bg-slate-900 bg-opacity-0 opacity-0 duration-300 hover:bg-opacity-40 hover:opacity-100 `}
         >
